82ea855bba6c2be4d5e88f783dfcb4716d8cf871
Sef <155701785+m-sef@users.noreply.github.com>
2025-02-04 23:20:10 -0500

    Updated Style + Changed Functionality

--- camera.gd
+++ camera.gd
@@ -1,39 +1,36 @@
-class_name Camera3DShake
+class_name ShakyCamera3D
 extends Camera3D
 
+#region Constants
 enum ShakeScale {
 	LINEAR = 1,
 	SQUARED = 2,
 	CUBED = 3
 }
 
-#region Constants
 const MIN_TRAUMA : float = 0.0
 const MAX_TRAUMA : float = 1.0
-const MIN_MOUSE_SENSITIVITY : float = 0.0
-const MAX_MOUSE_SENSITIVITY : float = 1.0
-const MIN_FOLLOW_WEIGHT : float = 0.001
 const MIN_SHAKE : float = 0.0
 const MAX_SHAKE : float = 1.0
+const LERP_WEIGHT : float = 10.0
+## It would be very, very awesome if this was in Godot by default.
 const UINT_MAX : int = 0x7fffffff
 #endregion
+
 #region Exports
 @export var shake_scale : ShakeScale = ShakeScale.CUBED
 
-@export var intensity : Vector3 = Vector3.ONE :
+@export_range(0.0, 100.0, 0.01) var shake_speed : float = 1.0;
+
+@export var shake_intensity : Vector3 = Vector3.ONE :
 	set = set_shake_intensity, get = get_shake_intensity
 
 @export_range(MIN_TRAUMA, MAX_TRAUMA, 0.01) var trauma_decay : float = 0.5 :
 	set = set_trauma_decay, get = get_trauma_decay
 
 @export_range(MIN_TRAUMA, MAX_TRAUMA, 0.01) var passive_trauma : float = 0.0
-
-@export_range(MIN_MOUSE_SENSITIVITY, MAX_MOUSE_SENSITIVITY) var mouse_sensitivity : float = 0.01 :
-	set = set_mouse_sensitivity, get = get_mouse_sensitivity
-
-@export var follow_weight : float = 10.0 :
-	set = set_follow_weight, get = get_follow_weight
 #endregion
+
 #region Public Variables
 var target_fov : float = fov :
 	set = set_target_fov, get = get_target_fov
@@ -44,94 +41,87 @@ var target_rotation : Vector3 = Vector3.ZERO :
 var shake : float = 0.0 :
 	set = set_shake, get = get_shake
 #endregion
+
 #region Private Variables
 @onready var _trauma : float = passive_trauma
 @onready var _noise : FastNoiseLite = FastNoiseLite.new()
 #endregion
-#region Set Functions
+
+#region Set/Get Functions
 func set_shake_intensity(value : Vector3) -> void:
-	intensity = value
+	shake_intensity = value
+
 
 func set_trauma_decay(value : float) -> void:
 	trauma_decay = clampf(value, MIN_TRAUMA, MAX_TRAUMA)
 
-func set_mouse_sensitivity(value : float) -> void:
-	mouse_sensitivity = clampf(value, MIN_MOUSE_SENSITIVITY, MAX_MOUSE_SENSITIVITY)
-
-func set_follow_weight(value : float) -> void:
-	follow_weight = maxf(MIN_FOLLOW_WEIGHT, value)
 
 func set_target_fov(value : float) -> void:
 	target_fov = value
 
+
 func set_target_rotation(value : Vector3) -> void:
 	target_rotation = value
 
+
 func set_shake(value : float):
 	shake = clampf(value, MIN_SHAKE, MAX_SHAKE)
-#endregion
-#region Get Functions
+
+
 func get_shake_intensity() -> Vector3:
-	return intensity
+	return shake_intensity
+
 
 func get_trauma_decay() -> float:
 	return trauma_decay
 
-func get_mouse_sensitivity() -> float:
-	return mouse_sensitivity
-
-func get_follow_weight() -> float:
-	return follow_weight
 
 func get_target_fov() -> float:
 	return target_fov
 
+
 func get_target_rotation() -> Vector3:
 	return target_rotation
+
 
 func get_shake() -> float:
 	return shake
 #endregion
 
+func _ready() -> void:
+	## HACK Applying a bitmask to the seed to prevent it from being a negative value
+	## When the seed is negative, the camera shake acts unpredictably
+	_noise.set_seed(randi() % UINT_MAX)
+	_noise.set_noise_type(FastNoiseLite.TYPE_PERLIN)
+
+
+func _physics_process(delta : float) -> void:
+	if _trauma:
+			_trauma = maxf(_trauma - trauma_decay * delta, passive_trauma)
+			_camera_shake()
+	
+	## Clamp the camera's x rotation to be within the range -90° to 90°
+	target_rotation.x = clamp(target_rotation.x, -PI/2, PI/2)
+	
+	## Gradually reset the camera's z rotation
+	target_rotation.z = lerpf(target_rotation.z, 0.0, LERP_WEIGHT * delta)
+	rotation = lerp(rotation, target_rotation, LERP_WEIGHT * delta)
+	fov = lerp(fov, target_fov, 10.0 * delta)
+
+
 func add_trauma(amount : float) -> void:
 	_trauma = minf(_trauma + amount, MAX_TRAUMA)
+
 
 func clear_trauma() -> void:
 	_trauma = passive_trauma
 
+
 func _camera_shake() -> void:
 	shake = pow(_trauma, shake_scale)
 	
-	var tick : int = Time.get_ticks_msec()
+	var tick : int = Time.get_ticks_msec() * shake_speed
 	
-	target_rotation.x += (shake * _noise.get_noise_2d(_noise.seed * 1, tick) * intensity.x)
-	target_rotation.y += (shake * _noise.get_noise_2d(_noise.seed * 2, tick) * intensity.y)
-	target_rotation.z += (shake * _noise.get_noise_2d(_noise.seed * 3, tick) * intensity.z)
-
-func _ready() -> void:
-	randomize()
-	# HACK Applying a bitmask to the seed to prevent it from being a negative value
-	# When the seed is negative, the camera shake acts unpredictably
-	_noise.set_seed(randi() % UINT_MAX)
-	_noise.set_noise_type(FastNoiseLite.TYPE_PERLIN)
-	
-	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
-
-func _unhandled_input(event : InputEvent) -> void:
-	if event is InputEventMouseMotion:
-		target_rotation.x += -event.relative.y * mouse_sensitivity
-		target_rotation.y += -event.relative.x * mouse_sensitivity
-
-func _physics_process(delta : float) -> void:
-	if _trauma:
-		_trauma = maxf(_trauma - trauma_decay * delta, passive_trauma)
-		_camera_shake()
-	
-	# Clamp the camera's x rotation to be within the range -90° to 90°
-	target_rotation.x = clamp(target_rotation.x, -PI/2, PI/2)
-	
-	# Gradually reset the camera's z rotation
-	target_rotation.z = lerpf(target_rotation.z, 0.0, follow_weight * delta)
-	
-	rotation = lerp(rotation, target_rotation, follow_weight * delta)
-	fov = lerp(fov, target_fov, 10.0 * delta)
+	rotation.x += (shake * _noise.get_noise_2d(_noise.seed * 1, tick) * shake_intensity.x)
+	rotation.y += (shake * _noise.get_noise_2d(_noise.seed * 2, tick) * shake_intensity.y)
+	rotation.z += (shake * _noise.get_noise_2d(_noise.seed * 3, tick) * shake_intensity.z)
--- main.gd
+++ main.gd
@@ -1,10 +1,16 @@
 extends Node3D
 
+const MOUSE_SENSITIVITY : float = 0.004
+
 @onready var debug_label : Label = $Control/MarginContainer/VBoxContainer/HBoxContainer/Label
+@onready var camera : ShakyCamera3D = $Camera3D
 
-@onready var camera : Camera3DShake = $Camera3D
 
-func _unhandled_input(event):
+func _ready() -> void:
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+
+
+func _unhandled_input(event : InputEvent):
 	if event.is_action_pressed("ui_cancel"):
 		get_tree().quit()
 	
@@ -13,14 +19,17 @@ func _unhandled_input(event):
 			camera.target_fov -= 4
 		if event.button_index == MOUSE_BUTTON_WHEEL_DOWN:
 			camera.target_fov += 4
+	
+	if event is InputEventMouseMotion:
+		camera.target_rotation.x += -event.relative.y * MOUSE_SENSITIVITY
+		camera.target_rotation.y += -event.relative.x * MOUSE_SENSITIVITY
 
-func _process(delta) -> void:
+
+func _process(delta : float) -> void:
 	if Input.is_action_pressed("ui_accept"):
 		camera.add_trauma(1.0 * delta)
 	
-	debug_label.text = "Shake: " + str(camera.get_shake()).pad_decimals(3)
-	debug_label.text += "\nScale: " + str(camera.shake_scale)
-	debug_label.text += "\nIntensity: " + str(camera.intensity)
-	debug_label.text += "\nTrauma: " + str(camera._trauma).pad_decimals(3)
-	debug_label.text += "\nTrauma Decay: " + str(camera.trauma_decay)
-	debug_label.text += "\nPassive Trauma: " + str(camera.passive_trauma)
+	debug_label.text =  "Camera Shake         : %s\n" % str(camera.get_shake()).pad_decimals(3)
+	debug_label.text += "Camera Trauma        : %s\n" % str(camera._trauma).pad_decimals(3)
+	debug_label.text += "Camera Trauma Decay  : %s\n" % str(camera.trauma_decay).pad_decimals(3)
+	debug_label.text += "Camera Passive Trauma: %s\n" % str(camera.passive_trauma).pad_decimals(3)
